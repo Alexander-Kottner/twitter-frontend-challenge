ad9f76a3d49c4fcbc9cb07ba9997d5c6887141d0
Alexander-Kottner <akottner@mail.austral.edu.ar>
2025-06-19 14:24:13 -0300

    typescript tweet-box

--- src/components/tweet-box/TweetBox.tsx
+++ src/components/tweet-box/TweetBox.tsx
@@ -1,48 +1,57 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import Button from "../button/Button";
 import TweetInput from "../tweet-input/TweetInput";
-import {useHttpRequestService} from "../../service/HttpRequestService";
-import {setLength, updateFeed} from "../../redux/user";
+import { useHttpRequestService } from "../../service/HttpRequestService";
+import { setLength, updateFeed } from "../../redux/user";
 import ImageContainer from "../tweet/tweet-image/ImageContainer";
-import {BackArrowIcon} from "../icon/Icon";
+import { BackArrowIcon } from "../icon/Icon";
 import ImageInput from "../common/ImageInput";
-import {useTranslation} from "react-i18next";
-import {ButtonType} from "../button/StyledButton";
-import {StyledTweetBoxContainer} from "./TweetBoxContainer";
-import {StyledContainer} from "../common/Container";
-import {StyledButtonContainer} from "./ButtonContainer";
-import {useDispatch, useSelector} from "react-redux";
-import {User} from "../../service";
+import { useTranslation } from "react-i18next";
+import { ButtonType } from "../button/StyledButton";
+import { StyledTweetBoxContainer } from "./TweetBoxContainer";
+import { StyledContainer } from "../common/Container";
+import { StyledButtonContainer } from "./ButtonContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { User } from "../../service";
+import { RootState } from "../../redux/store";
 
-const TweetBox = (props) => {
-    const {parentId, close, mobile, isCommentModal} = props;
-    const [content, setContent] = useState("");
-    const [images, setImages] = useState([]);
-    const [imagesPreview, setImagesPreview] = useState([]);
+interface TweetBoxProps {
+    parentId?: string;
+    close?: () => void;
+    mobile?: boolean;
+    isCommentModal?: boolean;
+    borderless?: boolean;
+}
 
-    const {length, query} = useSelector((state) => state.user);
+const TweetBox: React.FC<TweetBoxProps> = (props) => {
+    const { parentId, close, mobile, isCommentModal } = props;
+    const [content, setContent] = useState<string>("");
+    const [images, setImages] = useState<File[]>([]);
+    const [imagesPreview, setImagesPreview] = useState<string[]>([]);
+
+    const { length, query } = useSelector((state: RootState) => state.user);
     const httpService = useHttpRequestService();
     const dispatch = useDispatch();
-    const {t} = useTranslation();
-    const service = useHttpRequestService()
-    const [user, setUser] = useState()
-
+    const { t } = useTranslation();
+    const service = useHttpRequestService();
+    const [user, setUser] = useState<User | undefined>();
 
     useEffect(() => {
-        handleGetUser().then(r => setUser(r))
+        handleGetUser().then(r => setUser(r));
     }, []);
 
-    const handleGetUser = async () => {
-        return await service.me()
-    }
+    const handleGetUser = async (): Promise<User> => {
+        return await service.me();
+    };
 
-    const handleChange = (e) => {
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setContent(e.target.value);
     };
-    const handleSubmit = async () => {
+
+    const handleSubmit = async (): Promise<void> => {
         try {
             // Create the post first
-            const postData = {
+            const postData: { content: string; parentId?: string } = {
                 content: content
             };
             
@@ -59,7 +68,7 @@ const TweetBox = (props) => {
                     const fileExt = image.name.split('.').pop();
                     
                     // Get the S3 upload URL
-                    const uploadUrlResponse = await httpService.getImageUploadUrl(createdPost.id, fileExt, i);
+                    const uploadUrlResponse = await httpService.getImageUploadUrl(createdPost.id, fileExt!, i);
                     
                     // Upload to S3
                     await httpService.uploadImageToS3(uploadUrlResponse.uploadUrl, image);
@@ -107,14 +116,14 @@ const TweetBox = (props) => {
         }
     };
 
-    const handleRemoveImage = (index) => {
+    const handleRemoveImage = (index: number): void => {
         const newImages = images.filter((i, idx) => idx !== index);
         const newImagesPreview = newImages.map((i) => URL.createObjectURL(i));
         setImages(newImages);
         setImagesPreview(newImagesPreview);
     };
 
-    const handleAddImage = (newImages) => {
+    const handleAddImage = (newImages: File[]): void => {
         setImages(newImages);
         const newImagesPreview = newImages.map((i) => URL.createObjectURL(i));
         setImagesPreview(newImagesPreview);
